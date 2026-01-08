d9c9cecc97438d0ec80544fafc6a60134f50ff4a
Reggis94 <38286073+Reggis94@users.noreply.github.com>
2026-01-08 21:47:16 +0100

    Avoid soft-deleting service categories already deleted (#1)
    * Avoid soft-deleting service categories already deleted
    
    * Make service category code PSR-12 compliant

--- src/Services/Infrastructure/Dao/ServiceCategoryDao.php
+++ src/Services/Infrastructure/Dao/ServiceCategoryDao.php
@@ -2,9 +2,9 @@
 
 namespace App\Services\Infrastructure\Dao;
 
+use App\Services\Application\ServiceCategory\Dao\ServiceCategoryDaoInterface;
 use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Types\Types;
-use App\Services\Application\ServiceCategory\Dao\ServiceCategoryDaoInterface;
 
 final class ServiceCategoryDao implements ServiceCategoryDaoInterface
 {
@@ -18,11 +18,11 @@ final class ServiceCategoryDao implements ServiceCategoryDaoInterface
             'INSERT INTO service_category (pro_id, name) VALUES (:pro_id, :name)',
             [
                 'pro_id' => $proId,
-                'name'   => $name,
+                'name' => $name,
             ],
             [
                 'pro_id' => Types::INTEGER,
-                'name'   => Types::STRING,
+                'name' => Types::STRING,
             ]
         );
     }
@@ -30,12 +30,12 @@ final class ServiceCategoryDao implements ServiceCategoryDaoInterface
     public function delete(int $id): void
     {
         $this->connection->executeStatement(
-            'UPDATE service_category SET deleted_at = NOW() WHERE id = :id',
+            'UPDATE service_category SET deleted_at = NOW() WHERE id = :id AND deleted_at IS NULL',
             [
-                'id' => $id
+                'id' => $id,
             ],
             [
-                'id' => Types::INTEGER
+                'id' => Types::INTEGER,
             ]
         );
     }
@@ -43,13 +43,13 @@ final class ServiceCategoryDao implements ServiceCategoryDaoInterface
     public function doesBelongToPro(int $id, int $proId): bool
     {
         return (bool) $this->connection->fetchOne(
-            'SELECT 1 FROM service_category WHERE id = :id AND pro_id = :pro_id',
+            'SELECT 1 FROM service_category WHERE id = :id AND pro_id = :pro_id AND deleted_at IS NULL',
             [
-                'id'     => $id,
+                'id' => $id,
                 'pro_id' => $proId,
             ],
             [
-                'id'     => Types::INTEGER,
+                'id' => Types::INTEGER,
                 'pro_id' => Types::INTEGER,
             ]
         );
